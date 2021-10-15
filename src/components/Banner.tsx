39244303d63189850903327d6a68c00dace9197c
Roko Udovč <roko.udovc@outlook.com>
2021-10-15 10:48:21 +0200

    local storage za fav banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IProps } from "./component-interfaces";
 import Button from "@mui/material/Button";
 import ButtonBase from "@mui/material/ButtonBase";
@@ -5,26 +6,51 @@ import Typography from "@mui/material/Typography";
 import styles from "./banner.module.scss";
 
 export const Banner = (props: IProps): JSX.Element => {
-  return (
-    <div className={styles.homepagebar}>
-      <div className={styles.homepagebartext}>
-        <Typography
-          variant="h5"
-          style={{ color: "#ffffff", fontWeight: "bold" }}
-        >
-          Make MyNews your homepage
-        </Typography>
-        <Typography variant="h5" style={{ color: "#ffffff", fontWeight: 100 }}>
-          Every day discover what's trending on the internet!
-        </Typography>
-      </div>
-      <div className={styles.homepagebuttons}>
-        <Button className={styles.getbutton} variant="contained">
-          GET
-        </Button>
-        {/* Kako sredit hover tu a da zadrzin animaciju */}
-        <ButtonBase className={styles.nothanksbutton}>No, thanks</ButtonBase>
-      </div>
-    </div>
+  // Pametniji nacin za pratit ovo?
+  const isRejectedStorage =
+    localStorage.getItem("isRejected") === null
+      ? "false"
+      : localStorage.getItem("isRejected");
+
+  const [isRejected, setIsRejected] = useState(
+    isRejectedStorage === "false" ? false : true
   );
+
+  const handleClick = () => {
+    localStorage.setItem("isRejected", "true");
+    setIsRejected(true);
+  };
+
+  if (isRejected) {
+    return <div></div>;
+  } else {
+    return (
+      <div className={styles.homepagebar}>
+        <div className={styles.homepagebartext}>
+          <Typography
+            variant="h5"
+            style={{ color: "#ffffff", fontWeight: "bold" }}
+          >
+            Make MyNews your homepage
+          </Typography>
+          <Typography
+            variant="h5"
+            style={{ color: "#ffffff", fontWeight: 100 }}
+          >
+            Every day discover what's trending on the internet!
+          </Typography>
+        </div>
+        <div className={styles.homepagebuttons}>
+          {/*Kako napravit da ustvari bude homepage?*/}
+          <Button className={styles.getbutton} variant="contained">
+            GET
+          </Button>
+          {/* Kako sredit hover tu a da zadrzin animaciju */}
+          <ButtonBase className={styles.nothanksbutton} onClick={handleClick}>
+            No, thanks
+          </ButtonBase>
+        </div>
+      </div>
+    );
+  }
 };
